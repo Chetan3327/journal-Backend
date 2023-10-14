17ca8b07128b624dd7738abd2dc0cec83b7b5dc0
tanmayGypt <tanmay631@gmail.com>
2023-10-15 01:54:19 +0530

    message

--- server.js
+++ server.js
@@ -36,146 +36,148 @@ let contacts = [
   },
 ];
 
-let advisory_board = [
-  {
-    Name: "HON’BLE SH. ATAM PRAKASH KAUSHIK",
-    Designation: "CHAIRMAN",
-    Institute: "Bhagwan Parshuram Institute of Technology Delhi",
-    position: "Chief Patron",
-  },
-  {
-    Name: "SH. SURENDRA SHARMA",
-    Designation: "Vice President",
-    Institute: "Bhagwan Parshuram Institute of Technology Delhi.",
-    position: "Patron",
-  },
-  {
-    Name: "SH. VINOD VATS",
-    Designation: "General Secretary",
-    Institute: "Bhagwan Parshuram Institute of Technology Delhi",
-    position: "Patron",
-  },
-  {
-    Name: "PROF. PAYAL PAHWA",
-    Designation: "PRINCIPAL",
-    Institute: "Bhagwan Parshuram Institute of Technology Delhi",
-    position: "Editor-in-Chief ",
-  },
-  {
-    Name: "PROF. A. K. TANDON",
-    Designation: "Founder Director",
-    Institute: "Bhagwan Parshuram Institute of Technology Delhi",
-    position: "Advisor",
-  },
-  {
-    Name: "PROF. Y.D GAUR",
-    Designation: "EXECUTIVE",
-    Institute: "Bhagwan Parshuram Institute of Technology Delhi",
-    position: "Advisor",
-  },
-];
+let board = {
+  advisory_board: [
+    {
+      Name: "HON’BLE SH. ATAM PRAKASH KAUSHIK",
+      Designation: "CHAIRMAN",
+      Institute: "Bhagwan Parshuram Institute of Technology Delhi",
+      position: "Chief Patron",
+    },
+    {
+      Name: "SH. SURENDRA SHARMA",
+      Designation: "Vice President",
+      Institute: "Bhagwan Parshuram Institute of Technology Delhi.",
+      position: "Patron",
+    },
+    {
+      Name: "SH. VINOD VATS",
+      Designation: "General Secretary",
+      Institute: "Bhagwan Parshuram Institute of Technology Delhi",
+      position: "Patron",
+    },
+    {
+      Name: "PROF. PAYAL PAHWA",
+      Designation: "PRINCIPAL",
+      Institute: "Bhagwan Parshuram Institute of Technology Delhi",
+      position: "Editor-in-Chief ",
+    },
+    {
+      Name: "PROF. A. K. TANDON",
+      Designation: "Founder Director",
+      Institute: "Bhagwan Parshuram Institute of Technology Delhi",
+      position: "Advisor",
+    },
+    {
+      Name: "PROF. Y.D GAUR",
+      Designation: "EXECUTIVE",
+      Institute: "Bhagwan Parshuram Institute of Technology Delhi",
+      position: "Advisor",
+    },
+  ],
 
-const members = [
-  {
-    Name: "Dr. Abid Haleem",
-    Designation: "Professor",
-    Institute:
-      "Department of Mechanical Engineering, College of Engineering and Technology, Jamia Millia Islamia, New Delhi",
-  },
-  {
-    Name: "Dr S K Garg",
-    Designation: "Professor",
-    Institute:
-      "Department of Management Studies, Delhi Technological University, Delhi",
-  },
-  {
-    Name: "Dr. Ajai Agrawal",
-    Designation: "Professor",
-    Institute:
-      "Department of Mechanical Engineering, GB Pant University, Pantnagar",
-  },
-  {
-    Name: "Dr. Sushil",
-    Designation: "Professor",
-    Institute: "Department of Management Studies, IIT-Delhi",
-  },
-  {
-    Name: "Dr. M C Gupta",
-    Designation: "Professor",
-    Institute: " Department of Management Studies, IIT-Delhi",
-  },
-  {
-    Name: "Mr. Yatindra Sharma",
-    Designation: "Professor",
-    Institute:
-      "Department of Mechanical Engineering, College of Engineering and Technology, Jamia Millia Islamia, New Delhi",
-  },
-  {
-    Name: "Mr. Saumitra Sharma",
-    Designation: "PGDM (I I M Ahmadabad)",
-    Institute: "PGDM (I I M Ahmadabad), Professional U S A",
-  },
-  {
-    Name: "Mr. Shantanu Sharma",
-    Designation: "PGDM (I I M Ahmadabad)",
-    Institute: "PGDM (I I M Ahmadabad), Professional U S A",
-  },
-  {
-    Name: "Professor Chin-Chen Chang",
-    Designation: "PROFESSOR",
-    Institute:
-      "Department of information Engineering and Computer Science, Feng Chia University, Taiwan",
-  },
-  {
-    Name: "Dr Rana Singh",
-    Designation: "Assistant Director",
-    Institute:
-      "Institutional Research and Planning Gulf Medical University, Ajman, UAE",
-  },
-  {
-    Name: "Prof. Arif Suhail",
-    Designation: "PROFESSOR",
-    Institute:
-      " Department of Mechanical Engineering, Aligarh Muslim University",
-  },
-  {
-    Name: "Prof. Sanjeev Mittal",
-    Designation: "PROFESSOR",
-    Institute:
-      " University School of Management Studies, Guru Gobind Singh Indraprastha University, Delhi",
-  },
-  {
-    Name: "Dr. Rajeev Jain",
-    Designation: "Professor & Director of Chemistry",
-    Institute: "Institute of Engineering, Gwalior",
-  },
-  {
-    Name: "Dr. Neeraj Kaushik",
-    Designation: "Associate Professor",
-    Institute: "NIT, Kurukshetra",
-  },
-  {
-    Name: "Dr. Chitra Sharma",
-    Designation: "Professor",
-    Institute: "(Formerly at IGTUW, Delhi) Philadelphia (U S A)",
-  },
-  {
-    Name: "Prof. Rajiv Sharma",
-    Designation: "Professor",
-    Institute:
-      "Deputy Director and HOD (ECE) Bhagwan Parshuram Institute of Technology New Delhi.",
-  },
-  {
-    Name: "Prof. Achal Kaushik",
-    Designation: "Professor and Dean Academics",
-    Institute: "Bhagwan Parshuram Institute of Technology New Delhi.",
-  },
-  {
-    Name: "Prof. Achal Kaushik",
-    Designation: "Professor and HOD (IT)",
-    Institute: "Bhagwan Parshuram Institute of Technology New Delhi.",
-  },
-];
+  members: [
+    {
+      Name: "Dr. Abid Haleem",
+      Designation: "Professor",
+      Institute:
+        "Department of Mechanical Engineering, College of Engineering and Technology, Jamia Millia Islamia, New Delhi",
+    },
+    {
+      Name: "Dr S K Garg",
+      Designation: "Professor",
+      Institute:
+        "Department of Management Studies, Delhi Technological University, Delhi",
+    },
+    {
+      Name: "Dr. Ajai Agrawal",
+      Designation: "Professor",
+      Institute:
+        "Department of Mechanical Engineering, GB Pant University, Pantnagar",
+    },
+    {
+      Name: "Dr. Sushil",
+      Designation: "Professor",
+      Institute: "Department of Management Studies, IIT-Delhi",
+    },
+    {
+      Name: "Dr. M C Gupta",
+      Designation: "Professor",
+      Institute: " Department of Management Studies, IIT-Delhi",
+    },
+    {
+      Name: "Mr. Yatindra Sharma",
+      Designation: "Professor",
+      Institute:
+        "Department of Mechanical Engineering, College of Engineering and Technology, Jamia Millia Islamia, New Delhi",
+    },
+    {
+      Name: "Mr. Saumitra Sharma",
+      Designation: "PGDM (I I M Ahmadabad)",
+      Institute: "PGDM (I I M Ahmadabad), Professional U S A",
+    },
+    {
+      Name: "Mr. Shantanu Sharma",
+      Designation: "PGDM (I I M Ahmadabad)",
+      Institute: "PGDM (I I M Ahmadabad), Professional U S A",
+    },
+    {
+      Name: "Professor Chin-Chen Chang",
+      Designation: "PROFESSOR",
+      Institute:
+        "Department of information Engineering and Computer Science, Feng Chia University, Taiwan",
+    },
+    {
+      Name: "Dr Rana Singh",
+      Designation: "Assistant Director",
+      Institute:
+        "Institutional Research and Planning Gulf Medical University, Ajman, UAE",
+    },
+    {
+      Name: "Prof. Arif Suhail",
+      Designation: "PROFESSOR",
+      Institute:
+        " Department of Mechanical Engineering, Aligarh Muslim University",
+    },
+    {
+      Name: "Prof. Sanjeev Mittal",
+      Designation: "PROFESSOR",
+      Institute:
+        " University School of Management Studies, Guru Gobind Singh Indraprastha University, Delhi",
+    },
+    {
+      Name: "Dr. Rajeev Jain",
+      Designation: "Professor & Director of Chemistry",
+      Institute: "Institute of Engineering, Gwalior",
+    },
+    {
+      Name: "Dr. Neeraj Kaushik",
+      Designation: "Associate Professor",
+      Institute: "NIT, Kurukshetra",
+    },
+    {
+      Name: "Dr. Chitra Sharma",
+      Designation: "Professor",
+      Institute: "(Formerly at IGTUW, Delhi) Philadelphia (U S A)",
+    },
+    {
+      Name: "Prof. Rajiv Sharma",
+      Designation: "Professor",
+      Institute:
+        "Deputy Director and HOD (ECE) Bhagwan Parshuram Institute of Technology New Delhi.",
+    },
+    {
+      Name: "Prof. Achal Kaushik",
+      Designation: "Professor and Dean Academics",
+      Institute: "Bhagwan Parshuram Institute of Technology New Delhi.",
+    },
+    {
+      Name: "Prof. Achal Kaushik",
+      Designation: "Professor and HOD (IT)",
+      Institute: "Bhagwan Parshuram Institute of Technology New Delhi.",
+    },
+  ],
+};
 
 const mongoose = require("mongoose");
 mongoose.connect(`${process.env.mongourl}`, {
@@ -194,12 +196,8 @@ app.get("/contacts", (req, res) => {
   res.send(contacts);
 });
 
-app.get("/board/advisoryboard", (req, res) => {
-  res.send(advisory_board);
-});
-
-app.get("/board/members", (req, res) => {
-  res.send(members);
+app.get("/board", (req, res) => {
+  res.send(board);
 });
 
 let port = process.env.PORT || 3000;
