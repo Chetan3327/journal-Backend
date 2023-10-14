05db71287779ccfbcfbb6ce4e1a828fda8d72edf
tanmayGypt <tanmay631@gmail.com>
2023-10-15 00:24:28 +0530

    message

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require("path");
 // app.use(express.static("public"));
 require("dotenv").config();
 app.use(express.json());
-
+app.use(cors());
 const mongoose = require("mongoose");
 mongoose.connect(`${process.env.mongourl}`, {
   useNewUrlParser: true,
